b16a28b305d373f9625236f638ac907047b6c707
Talonflame23185 <connorottbaseball@gmail.com>
2025-06-04 12:20:37 -0700

    Initial Commit

---
+++ FetchMobileTakeHomeProject_ConnorOtt/FetchMobileTakeHomeProject_ConnorOttApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchMobileTakeHomeProject_ConnorOttApp.swift
+//  FetchMobileTakeHomeProject_ConnorOtt
+//
+//  Created by Connor Ott on 6/4/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchMobileTakeHomeProject_ConnorOttApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
